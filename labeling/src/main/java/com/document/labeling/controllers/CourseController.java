e93900a8a9705e3fe30e836df45945b8ebb51b27
Jacob Hoffman <cubhoffman@gmail.com>
2024-10-27 09:31:18 -0500

    add: initial CourseController class

---
+++ labeling/src/main/java/com/document/labeling/controllers/CourseController.java
@@ -0,0 +1,41 @@
+package com.document.labeling.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.document.labeling.models.Course;
+import com.document.labeling.services.CourseService;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping("/courses")
+    public List<Course> getCourse() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/courses/{id}")
+    public Course getCourseById(@PathVariable String id) {
+        return courseService.getCourseById(id);
+    }
+
+    @PostMapping("/courses")
+    public Course postCourse(final @RequestBody Course course) {
+        return courseService.postCourse(course);
+    }
+
+    @DeleteMapping("/courses")
+    public void deleteAllCourses() {
+        courseService.deleteAllCourses();
+    }
+}
