3c4360d6c73740d7d9e04c79c20b49ebe9938267
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-08 15:54:51 -0600

    add: CourseRepository interface

--- labeling/src/main/java/com/document/labeling/repositories/CourseRepository.java
+++ labeling/src/main/java/com/document/labeling/repositories/CourseRepository.java
@@ -8,5 +8,5 @@ import com.document.labeling.models.Course;
  * Repository used in storing course objects in database
  */
 public interface CourseRepository extends MongoRepository<Course, String> {
-
+    
 }
