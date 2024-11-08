3b748b574de4f02d4b402eecab28ab2b6cb5778b
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-08 15:53:09 -0600

    add: Course model class changes

--- labeling/src/main/java/com/document/labeling/models/Course.java
+++ labeling/src/main/java/com/document/labeling/models/Course.java
@@ -1,17 +1,31 @@
 package com.document.labeling.models;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "courses")
-public class Course {
+public class Course extends BaseModel {
+
     @Id
+    private String id;
     private String name;
     private String description;
 
-    public Course(String name, String description) {
+    public Course(String id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
+        this.setCreatedDate(LocalDateTime.now());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -31,38 +45,8 @@ public class Course {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Course other = (Course) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        return true;
-    }
-
-    @Override
     public String toString() {
-        return "Course [name=" + name + ", description=" + description + "]";
+        return "Course [id=" + id + ", name=" + name + ", description=" + description + "]";
     }
+
 }
