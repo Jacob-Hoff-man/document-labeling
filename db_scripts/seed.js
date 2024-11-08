9687c213bad85a9cf0e0b67225e89a9beb475384
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-08 11:01:07 -0600

    add: db seed

---
+++ db_scripts/seed.js
@@ -0,0 +1,7 @@
+// seed.js
+
+db.courses.insertMany([
+    { _id: 'seed-id-1', name: 'seed-course-1', description: 'seed-description-1' },
+    { _id: 'seed-id-2', name: 'seed-course-2', description: 'seed-description-2' },
+    { _id: 'seed-id-3', name: 'seed-course-3', description: 'seed-description-3' },
+]);
