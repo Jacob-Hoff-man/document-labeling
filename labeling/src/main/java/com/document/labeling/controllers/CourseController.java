f0772b77d588f6eac1217f8a530df11b96979d41
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-09 17:18:43 -0600

    ADD: update CourseController class to use ResponseStatus and return ResponseEntities

--- labeling/src/main/java/com/document/labeling/controllers/CourseController.java
+++ labeling/src/main/java/com/document/labeling/controllers/CourseController.java
@@ -3,16 +3,21 @@ package com.document.labeling.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.document.labeling.daos.CoursePostRequest;
 import com.document.labeling.models.Course;
-import com.document.labeling.models.request.CoursePostRequest;
 import com.document.labeling.services.CourseService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class CourseController {
@@ -21,21 +26,25 @@ public class CourseController {
     private CourseService courseService;
 
     @GetMapping("/courses")
+    @ResponseStatus(HttpStatus.OK)
     public List<Course> getCourses() {
         return courseService.getAllCourses();
     }
 
     @GetMapping("/courses/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Course getCourseById(@PathVariable String id) {
         return courseService.getCourseById(id);
     }
 
     @PostMapping("/courses")
-    public Course postCourse(final @RequestBody CoursePostRequest course) {
+    @ResponseStatus(HttpStatus.OK)
+    public Course postCourse(final @RequestBody @Valid CoursePostRequest course) {
         return courseService.postCourse(course);
     }
 
     @DeleteMapping("/courses")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteAllCourses() {
         courseService.deleteAllCourses();
     }
