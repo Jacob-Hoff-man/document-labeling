092520da1fa697cf0aeed2a3bf9bdae227255965
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-10 10:44:08 -0600

    remove unused exception

--- labeling/src/main/java/com/document/labeling/exceptions/course/CourseAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.document.labeling.exceptions.course;
-
-public class CourseAlreadyExistsException extends RuntimeException {
-    public CourseAlreadyExistsException(String message) {
-        super(message);
-    }
-}
