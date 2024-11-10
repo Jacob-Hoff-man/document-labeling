ca49aea9fc7d7bbd48bf4874d260a2f972c576d7
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-10 10:40:21 -0600

    ADD: CoursePutRequest Dao

---
+++ labeling/src/main/java/com/document/labeling/daos/CoursePutRequest.java
@@ -0,0 +1,32 @@
+package com.document.labeling.daos;
+
+public class CoursePutRequest {
+    private String name;
+    private String description;
+
+    public CoursePutRequest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "CoursePutRequest [name=" + name + ", description=" + description + "]";
+    }
+}
