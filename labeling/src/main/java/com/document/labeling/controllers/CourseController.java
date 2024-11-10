3fab7c3f7a50ea09386ce4df86b4464e679dac68
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-09 20:17:41 -0600

    refactor Course controller RequestMapping

--- labeling/src/main/java/com/document/labeling/controllers/CourseController.java
+++ labeling/src/main/java/com/document/labeling/controllers/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,32 +20,39 @@ import com.document.labeling.services.CourseService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/courses")
 public class CourseController {
 
     @Autowired
     private CourseService courseService;
 
-    @GetMapping("/courses")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Course> getCourses() {
         return courseService.getAllCourses();
     }
 
-    @GetMapping("/courses/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Course getCourseById(@PathVariable String id) {
         return courseService.getCourseById(id);
     }
 
-    @PostMapping("/courses")
+    @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public Course postCourse(final @RequestBody @Valid CoursePostRequest course) {
         return courseService.postCourse(course);
     }
 
-    @DeleteMapping("/courses")
+    @DeleteMapping
     @ResponseStatus(HttpStatus.OK)
     public void deleteAllCourses() {
+        courseService.deleteAllCourses();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCourseById(@PathVariable String id) {
         courseService.deleteAllCourses();
     }
 }
