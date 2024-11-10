154dc75321012fb340b568ba1efcba1defc6c92e
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-10 10:43:42 -0600

    ADD: CourseNotUpdatedException

---
+++ labeling/src/main/java/com/document/labeling/exceptions/course/CourseNotUpdatedException.java
@@ -0,0 +1,7 @@
+package com.document.labeling.exceptions.course;
+
+public class CourseNotUpdatedException extends RuntimeException {
+    public CourseNotUpdatedException(String message) {
+        super(message);
+    }
+}
