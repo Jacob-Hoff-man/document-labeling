fb66e626f394d0ba36bd584422f52a0400a21132
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-10 12:48:12 -0600

    CourseDocumentSentenceRecord model

---
+++ labeling/src/main/java/com/document/labeling/models/CourseDocumentSentenceRecord.java
@@ -0,0 +1,52 @@
+package com.document.labeling.models;
+
+import java.util.Arrays;
+
+import org.springframework.data.annotation.Id;
+
+import com.document.labeling.models.id.CourseDocumentSentenceRecordId;
+
+public class CourseDocumentSentenceRecord extends Base {
+    @Id
+    CourseDocumentSentenceRecordId documentSentenceRecordId;
+    private String[] words;
+    private String[] labels;
+
+    public CourseDocumentSentenceRecord(CourseDocumentSentenceRecordId documentSentenceRecordId, String[] words,
+            String[] labels) {
+        super();
+        this.documentSentenceRecordId = documentSentenceRecordId;
+        this.words = words;
+        this.labels = labels;
+    }
+
+    public CourseDocumentSentenceRecordId getDocumentSentenceRecordId() {
+        return documentSentenceRecordId;
+    }
+
+    public void setDocumentSentenceRecordId(CourseDocumentSentenceRecordId documentSentenceRecordId) {
+        this.documentSentenceRecordId = documentSentenceRecordId;
+    }
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
+    public String[] getLabels() {
+        return labels;
+    }
+
+    public void setLabels(String[] labels) {
+        this.labels = labels;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseDocumentSentenceRecord [documentSentenceRecordId=" + documentSentenceRecordId + ", words="
+                + Arrays.toString(words) + ", labels=" + Arrays.toString(labels) + "]";
+    }
+}
