d189b9d692bdea7fffa429f97c4cfae64f70de8e
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-11 13:23:56 -0600

    ADD: LabelConstants (B,I,O)

---
+++ labeling/src/main/java/com/document/labeling/constants/LabelConstants.java
@@ -0,0 +1,10 @@
+package com.document.labeling.constants;
+
+public class LabelConstants {
+    public static final String BEGINNING = "B";
+    public static final String INSIDE = "I";
+    public static final String OUTSIDE = "O";
+
+    private LabelConstants() {
+    }
+}
