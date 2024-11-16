d6f332459ec748aa7dd8f08abf8c4ff35fda3e74
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-15 22:54:23 -0600

    ADD: CourseDocumentSentenceRecord GET list fcn in controller class

--- labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
+++ labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,28 +27,37 @@ public class CourseDocumentSentenceRecordController {
     @Autowired
     private CourseDocumentSentenceRecordService courseDocumentSentenceRecordService;
 
+    @GetMapping("/{documentId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CourseDocumentSentenceRecord> getCourseDocumentSentenceRecordList(
+            @PathVariable String courseId,
+            @PathVariable String documentId) {
+        System.out.println("@@ " + courseId + " " + documentId);
+        return courseDocumentSentenceRecordService.getAllCourseDocumentSentenceRecords(courseId, documentId);
+    }
+
     // TODO: replace this with a POST CourseDocument endpoint, and internally use
     // service fcn
-    @PostMapping("/{id}")
+    @PostMapping("/{documentId}")
     @ResponseStatus(HttpStatus.OK)
     public List<CourseDocumentSentenceRecord> postCourseDocumentSentenceRecordList(
             @PathVariable String courseId,
-            @PathVariable String id,
+            @PathVariable String documentId,
             final @RequestBody @Valid CourseDocumentSentenceRecordListPostRequest request) {
-        return courseDocumentSentenceRecordService.postCourseDocumentSentenceRecordList(courseId, id, request);
+        return courseDocumentSentenceRecordService.postCourseDocumentSentenceRecordList(courseId, documentId, request);
     }
 
-    @PutMapping("/{documentId}/id")
+    @PutMapping("/{documentId}/{sentenceId}")
     @ResponseStatus(HttpStatus.OK)
     public CourseDocumentSentenceRecord putCourseDocumentSentenceRecordById(
             @PathVariable String courseId,
             @PathVariable String documentId,
-            @PathVariable int id,
+            @PathVariable int sentenceId,
             final @RequestBody @Valid CourseDocumentSentenceRecordPutRequest request) {
         return courseDocumentSentenceRecordService.putCourseDocumentSentenceRecordById(
                 courseId,
                 documentId,
-                id,
+                sentenceId,
                 request);
     }
 
