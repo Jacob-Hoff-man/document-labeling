978dcb379e32b0dd46828940ef555667210f89b7
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-16 00:10:34 -0600

    ADD: CourseDocumentSentenceRecord PUT constants

--- labeling/src/main/java/com/document/labeling/constants/CourseDocumentSentenceRecordConstants.java
+++ labeling/src/main/java/com/document/labeling/constants/CourseDocumentSentenceRecordConstants.java
@@ -9,4 +9,17 @@ public class CourseDocumentSentenceRecordConstants {
                 "CourseDocumentSentenceRecord not found with course ID : %s, document ID : %s, sentence ID : %s",
                 courseId, documentId, sentenceId);
     }
+
+    public static String courseDocumentSentenceRecordNotUpdated(String courseId, String documentId, int sentenceId) {
+        return String.format(
+                "CourseDocumentSentenceRecord not updated, but was found with course ID : %s, document ID: %s, sentence ID : %s",
+                courseId, documentId, sentenceId);
+    }
+
+    public static String courseDocumentSentenceRecordWordsAndLabelsLengthNotEqual(String courseId, String documentId,
+            int sentenceId) {
+        return String.format(
+                "CourseDocumentSentenceRecord with course ID : %s, document ID: %s, sentence ID : %s does not have matching length for words and labels property",
+                courseId, documentId, sentenceId);
+    }
 }
