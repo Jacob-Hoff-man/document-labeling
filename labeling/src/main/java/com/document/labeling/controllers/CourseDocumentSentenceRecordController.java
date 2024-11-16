efe21548892bca0cbe1156f4088c859d5584614d
Jacob Hoffman <cubhoffman@gmail.com>
2024-11-15 22:12:25 -0600

    ADD: CourseDocumentSentenceRecord Put fcn in controller class

--- labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
+++ labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.document.labeling.daos.CourseDocumentSentenceRecordListPostRequest;
+import com.document.labeling.daos.CourseDocumentSentenceRecordPutRequest;
 import com.document.labeling.models.CourseDocumentSentenceRecord;
 import com.document.labeling.services.CourseDocumentSentenceRecordService;
 
@@ -32,8 +34,21 @@ public class CourseDocumentSentenceRecordController {
             @PathVariable String courseId,
             @PathVariable String id,
             final @RequestBody @Valid CourseDocumentSentenceRecordListPostRequest request) {
-        System.out.println("@@post cdsrl " + courseId + " " + id + " " + request.toString());
         return courseDocumentSentenceRecordService.postCourseDocumentSentenceRecordList(courseId, id, request);
+    }
+
+    @PutMapping("/{documentId}/id")
+    @ResponseStatus(HttpStatus.OK)
+    public CourseDocumentSentenceRecord putCourseDocumentSentenceRecordById(
+            @PathVariable String courseId,
+            @PathVariable String documentId,
+            @PathVariable int id,
+            final @RequestBody @Valid CourseDocumentSentenceRecordPutRequest request) {
+        return courseDocumentSentenceRecordService.putCourseDocumentSentenceRecordById(
+                courseId,
+                documentId,
+                id,
+                request);
     }
 
 }
