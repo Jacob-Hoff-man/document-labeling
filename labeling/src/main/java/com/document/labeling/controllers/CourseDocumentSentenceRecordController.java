fdec746b87a5288dc0c41ab353e79c6293909346
Jacob Hoffman <cubhoffman@gmail.com>
2024-12-02 19:22:51 -0500

    add: CourseDocumentSentenceRecordController class updates

--- labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
+++ labeling/src/main/java/com/document/labeling/controllers/CourseDocumentSentenceRecordController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.document.labeling.dtos.CourseDocumentSentenceRecordListPostRequest;
 import com.document.labeling.dtos.CourseDocumentSentenceRecordPutRequest;
 import com.document.labeling.models.CourseDocumentSentenceRecord;
 import com.document.labeling.services.CourseDocumentSentenceRecordService;
@@ -44,17 +42,6 @@ public class CourseDocumentSentenceRecordController {
             @PathVariable String documentId) {
         return courseDocumentSentenceRecordService.getAllCourseDocumentSentenceRecords(courseId, documentId);
     }
-
-    // // TODO: replace this with a POST CourseDocument endpoint, and internally use
-    // // service fcn
-    // @PostMapping
-    // @ResponseStatus(HttpStatus.OK)
-    // public List<CourseDocumentSentenceRecord> postCourseDocumentSentenceRecordList(
-    //         @PathVariable String courseId,
-    //         @PathVariable String documentId,
-    //         final @RequestBody @Valid CourseDocumentSentenceRecordListPostRequest request) {
-    //     return courseDocumentSentenceRecordService.postCourseDocumentSentenceRecordList(courseId, documentId, request);
-    // }
 
     @PutMapping("/sentences/{sentenceId}")
     @ResponseStatus(HttpStatus.OK)
