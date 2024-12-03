f6c0fea5953dd7bbb6ea8c32b446c6d47309c20e
Jacob Hoffman <cubhoffman@gmail.com>
2024-12-03 13:31:31 -0500

    fix: CourseDocument not found constant message

--- labeling/src/main/java/com/document/labeling/constants/CourseDocumentConstants.java
+++ labeling/src/main/java/com/document/labeling/constants/CourseDocumentConstants.java
@@ -4,6 +4,12 @@ public class CourseDocumentConstants {
     private CourseDocumentConstants() {
     }
 
+    public static String courseDocumentNotFound(String courseId, String documentId) {
+        return String.format(
+                "CourseDocument not found with course ID : %s, document ID : %s",
+                courseId, documentId);
+    }
+
     public static String courseDocumentFileNotAccepted() {
         return String.format("CourseDocument PDF file not accepted");
     }
