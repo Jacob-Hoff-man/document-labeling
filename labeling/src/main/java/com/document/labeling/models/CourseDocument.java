9ac00edb2d70d533177c43b7a8f9ba755c050e52
Jacob Hoffman <cubhoffman@gmail.com>
2024-12-02 19:21:52 -0500

    add: CourseDocument model

--- labeling/src/main/java/com/document/labeling/models/CourseDocument.java
+++ labeling/src/main/java/com/document/labeling/models/CourseDocument.java
@@ -2,23 +2,25 @@ package com.document.labeling.models;
 
 import javax.persistence.Id;
 
+import com.document.labeling.models.id.CourseDocumentId;
+
 public class CourseDocument extends Base {
     @Id
-    private CourseDocument id;
+    private CourseDocumentId id;
     private String name;
     private String description;
 
-    public CourseDocument(CourseDocument id, String name, String description) {
+    public CourseDocument(CourseDocumentId id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public CourseDocument getId() {
+    public CourseDocumentId getId() {
         return id;
     }
 
-    public void setId(CourseDocument id) {
+    public void setId(CourseDocumentId id) {
         this.id = id;
     }
 
@@ -41,43 +43,6 @@ public class CourseDocument extends Base {
     @Override
     public String toString() {
         return "CourseDocument [id=" + id + ", name=" + name + ", description=" + description + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        CourseDocument other = (CourseDocument) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        return true;
     }
 
 }
